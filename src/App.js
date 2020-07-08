f678bc07a24e10b18335b04bc790bc0f456f6a7a
SushreeP <pratishruti.mohanty7@gmail.com>
2020-07-08 20:42:13 +0100

    update all files

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ function App() {
         <div className="details">
           <p className="name">Sushree Pratishruti Mohanty</p>
           <p className="most-recent-qualification">
-            MSc International Business Graduate from City University of London
-            Business School
+            MSc International Business Graduate <br />
+            from Cass Business School
           </p>
           <p className="more-details">
-            Looking for Software Developer Roles in UK and Ireland{" "}
-            <span className="bold">with</span> Visa Sponsorship
+            <span className="bold">Looking for</span> Software Developer Roles
+            in UK and Ireland
+            <br /> <span className="bold">with</span> Visa Sponsorship
             <br />
-            Bachelor's in Information Technology & 2yrs of Work Experience as
-            Systems Engineer with Infosys
+            Bachelor's in Information Technology
+            <br />
+            2yrs of Work Experience as Systems Engineer with Infosys
             <br />
             <br />
             <hr />
             React | Python | C++ | SQL | Quick understanding of new language |
-            Good with Data Structure
+            Good with Data Structure |{" "}
+            <a href={"#"}>other skills & certification...</a>
           </p>
         </div>
       </div>
